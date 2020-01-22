366190dfcfcd94ade50ac502c591d94cf7c7c0bf
Wini996 <libbywinfield101@gmail.com>
2020-01-22 19:47:06 +0000

    readded the mongodb

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("MONGO_DBNAME", "StudentLife")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://LWin_01:01Libby@studentLife-ldsrb.mongodb.net/StudentLife?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "student")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://LWin_01:01Libby@student-ldsrb.mongodb.net/student?retryWrites=true&w=majority")
 
 COLLECTION_NAME = "StudentLife"
