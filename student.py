c46e42f90ae060cdd82b9f1d220fb8568e8890b6
Wini996 <libbywinfield101@gmail.com>
2020-01-28 10:37:46 +0000

    connected t mongodb

--- student.py
+++ student.py
@@ -6,14 +6,20 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'StudentLife'
+app.config["MONGO_URI"] = 'mongodb+srv://LWin_01:01Libby@StudentLife-ldsrb.mongodb.net/StudentLife?retryWrites=true&w=majority'
+                                    
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", recipes=mongo.db.recipes.find())
 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("add_recipe.html", page_title="Add Recipe"
+    return render_template("add_recipe.html", page_title="Add Recipe",
     recipes=mongo.db.recipes.find())
+
     
 
 
