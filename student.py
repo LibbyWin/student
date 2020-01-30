88f5ac7bb0a063b001034d2c8cb39d59d9af92b4
Wini996 <libbywinfield101@gmail.com>
2020-01-30 17:43:59 +0000

    edit to color on the card bodies and boarder

--- student.py
+++ student.py
@@ -25,7 +25,7 @@ def add_recipe():
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    recipes = mongo.db.recipes
+    recipes=mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
 
