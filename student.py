f289c36cae9755c9d14e7419898f897696e9709b
Wini996 <libbywinfield101@gmail.com>
2020-02-05 15:52:46 +0000

    edit to the jinja on student.py and index.html

--- student.py
+++ student.py
@@ -11,16 +11,27 @@ app.config["MONGO_URI"] = 'mongodb+srv://LWin_01:01Libby@StudentLife-ldsrb.mongo
                                     
 mongo = PyMongo(app)
 
+
+
+
 @app.route('/')
+@app.route('/index')
 def index():
     recipes=mongo.db.recipes.find()
     categories=mongo.db.categories.find()
-    return render_template("index.html", recipes=recipes, categories=categories)
+    return render_template("index.html", recipes=recipes, categories=categories,)
+
+
+
 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("add_recipe.html", page_title="Add Recipe", recipes=mongo.db.recipes.find(),
+    return render_template("add_recipe.html", page_title="Add Recipe", 
+    recipes=mongo.db.recipes.find(),
     categories=mongo.db.categories.find())
+
+
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -28,11 +39,19 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
 
+
+
+
 @app.route('/view_recipe/<recipes_id>')
 def view_recipe(recipes_id):
    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
    all_category = mongo.db.category.find() 
+
    return render_template("view_recipe.html", recipes=the_recipe, category=all_category)
+
+
+
+
 
 if __name__ == "__main__":  
         app.run(host=os.environ.get("IP"),
