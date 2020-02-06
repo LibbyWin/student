2acfb716f4424e08f30aa1e258e8caeb088c2970
Wini996 <libbywinfield101@gmail.com>
2020-02-06 11:19:49 +0000

    added comments to the style sheet and student.py file - added a delete recipe section within student.py removing a recipe from mongo.db

--- student.py
+++ student.py
@@ -11,7 +11,7 @@ app.config["MONGO_URI"] = 'mongodb+srv://LWin_01:01Libby@StudentLife-ldsrb.mongo
                                     
 mongo = PyMongo(app)
 
-
+#Connects the home page to mongodb in order to show the recipes 
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,58 +19,60 @@ def index():
     categories=mongo.db.categories.find()
     return render_template("index.html", recipes=recipes, categories=categories,)
 
-
-
-
+#allowing you to add a whole new recipe to the database and site
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add_recipe.html", page_title="Add Recipe", 
     recipes=mongo.db.recipes.find(),
     categories=mongo.db.categories.find())
 
-
-
-
+#allowing you to add a whole new recipe to the database and site
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes=mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
 
-
-
+#Previewing the whole recipe information
 @app.route('/view_recipe/<recipes_id>')
 def view_recipe(recipes_id):
    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
    all_category = mongo.db.category.find() 
    return render_template("view_recipe.html", recipes=the_recipe, category=all_category)
 
+#editing recipe information
 @app.route('/edit_recipe/<recipes_id>')
 def edit_recipe(recipes_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
     all_category = mongo.db.category.find()
     return render_template("edit_recipe.html", recipes=the_recipe, category=all_category)
 
-@app.route('/update_recipe/<recipes_id>', method=["POST"])
+#Updating your recipes
+@app.route('/update_recipe/<recipes_id>', methods=["POST"])
 def update_recipe(recipes_id):
-    recipes = mongo.bd.recipes
+    recipes = mongo.db.recipes
     recipes.update( {'_id': ObjectId(recipes_id)},
     {
-            "name": request.from.get("name",)
-            "category_name": request.form.get("category"),
-            "serves": request.form.get("serves"),
-            "price": request.form.get("price"),
-            "protein": request.form.get("protien"),
-            "ingredients": list(request.form.get("ingredients").split("\n")),
-            #.split("/n") allows for all new inputs to be added on a new line.
-            "instructions": list(request.form.get("instructions").split("\n")),
-            "description": request.form.get("description"),
-            "image": request.form.get("image_url"),
+        #Allows for each new recipe to be given these names and to locate/contain the provided info within the names
+            "name": request.form.get["name"],
+            "category_name": request.form.get["category"],
+            "serves": request.form.get["serves"],
+            "price": request.form.get["price"],
+            "protein": request.form.get["protien"],
+            #.split("/n") allows for all new inputs to be added on a new line
+            "ingredients": list(request.form.get["ingredients"].split("\n")),
+            #.split("/n") allows for all new inputs to be added on a new line
+            "instructions": list(request.form.get["instructions"].split("\n")),
+            "description": request.form.get["description"],
+            "image": request.form.get["image_url"],
+    })
+    return redirect(url_for('index'))
 
-    }
-
-    )
-
+#For deleting recipes
+@app.route('/delete_recipe/<recipes_id>')
+def delete_recipe(recipes_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipes_id)})
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":  
         app.run(host=os.environ.get("IP"),
