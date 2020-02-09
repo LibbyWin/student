abc17ce0a29fc10fcf15bd8f739679cdd855ae5e
Wini996 <libbywinfield101@gmail.com>
2020-02-09 21:41:15 +0000

    edit to ignorgit

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("MONGO_DBNAME", "StudentLife")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://LWin_01:01Libby@StudentLife-ldsrb.mongodb.net/StudentLife?retryWrites=true&w=majority")
-os.environ["secret_key"] = "Secret"
-COLLECTION_NAME = "StudentLife"
-
-
